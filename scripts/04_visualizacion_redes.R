361234b86eb0b39181a11a8da54ca259c3145179
Irene <mahiques.irene@gmail.com>
2026-05-04 21:47:14 +0200

    Update 04_visualizacion_redes.R

--- scripts/04_visualizacion_redes.R
+++ scripts/04_visualizacion_redes.R
@@ -8,7 +8,7 @@ library(ggplot2)
 
 # 2. REDES BIPARTITAS (plotweb)
 # Esto genera el SVG que luego usas en Inkscape
-svg("output/Red_Bipartita_Final.pdf", width = 16, height = 10)
+svg("output/Red_Bipartita_Final.svg", width = 16, height = 10)
 plotweb(M_limpia, text.rot = 45, labsize = 2,
         col.high = "#3B5C11", col.low = "#542D0F", 
         y.width.low = 0.05, y.width.high = 0.05)
