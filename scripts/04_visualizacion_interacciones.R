122bac39760e109ad8aa18a49d825dd1e72314f4
Irene <mahiques.irene@gmail.com>
2026-05-04 22:03:05 +0200

    Update 04_visualizacion_interacciones.R

--- scripts/04_visualizacion_interacciones.R
+++ scripts/04_visualizacion_interacciones.R
@@ -1,9 +1,17 @@
 library(bipartite)
+library(ggplot2)
 library(svglite)
 
-# Generación de grafos bipartitos estéticos para edición en Inkscape
-# Configuración de escala: col.high = #3B5C11 (Micobionte), col.low = #542D0F (Fotobionte)
+# 1. RED BIPARTITA ESTÉTICA (Ejemplo Comunidad Valenciana)
+# Cargamos datos para el gráfico
+try({
+  df <- read.csv2("Comunidad Valenciana.csv")
+  matriz <- table(df$Micobionte, df$Fotobionte)
+  M_limpia <- matriz[rownames(matriz) != "", colnames(matriz) != ""]
 
-# svglite("output/Red_Bipartita_Final.svg", width = 16, height = 10)
-# plotweb(matriz, text.rot = 45, labsize = 1.2)
-# dev.off()
+  # Generamos el SVG para Inkscape
+  svglite("output/Red_Bipartita_Final.svg", width = 16, height = 10)
+  plotweb(M_limpia, text.rot = 45, labsize = 1.2,
+          col.high = "#3B5C11", col.low = "#542D0F", 
+          y.width.low = 0.05, y.width.high = 0.05)
+  dev.off()
