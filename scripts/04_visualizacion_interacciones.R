2006772080964882822109bc1415710a44b561aa
Irene <mahiques.irene@gmail.com>
2026-05-04 21:59:30 +0200

    Update and rename 04_visualizacion_redes.R to 04_visualizacion_interacciones.R

---
+++ scripts/04_visualizacion_interacciones.R
@@ -0,0 +1,9 @@
+library(bipartite)
+library(svglite)
+
+# Generación de grafos bipartitos estéticos para edición en Inkscape
+# Configuración de escala: col.high = #3B5C11 (Micobionte), col.low = #542D0F (Fotobionte)
+
+# svglite("output/Red_Bipartita_Final.svg", width = 16, height = 10)
+# plotweb(matriz, text.rot = 45, labsize = 1.2)
+# dev.off()
